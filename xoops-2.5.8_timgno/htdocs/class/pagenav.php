73f173d5b5762a5feb9bb29a9dbfe3f8e866e2c8
Txmod Xoops <txmodxoops@gmail.com>
2015-02-14 18:26:20 +0100

    Added Cpanel Xnewage Theme
    Beta testing of xnewage cpanel theme

---
+++ xoops-2.5.8_timgno/htdocs/class/pagenav.php
@@ -0,0 +1,218 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * XOOPS page navigation
+ *
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @package     class
+ * @since       2.5.6
+ * @author      Kazumi Ono (http://www.myweb.ne.jp/, http://jp.xoops.org/)
+ * @author      Gregory Mage (AKA Mage)
+ * @version     $Id: pagenav.php 11791 2013-06-28 10:13:50Z timgno $
+ */
+
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+class XoopsPageNav
+{
+    /**
+     * *#@+
+     *
+     * @access private
+     */
+    /**
+     * @var int
+     */
+    private $total;
+
+    /**
+     * @var int
+     */
+    private $perpage;
+
+    /**
+     * @var int
+     */
+    private $current;
+
+    /**
+     * @var string
+     */
+    private $url;
+    /**
+     * *#@-
+     */
+
+    /**
+     * Constructor
+     *
+     * @param int $total_items Total number of items
+     * @param int $items_perpage Number of items per page
+     * @param int $current_start First item on the current page
+     * @param string $start_name Name for "start" or "offset"
+     * @param string $extra_arg Additional arguments to pass in the URL
+     */
+    public function __construct($total_items, $items_perpage, $current_start, $start_name = "start", $extra_arg = "")
+    {
+        $this->total = intval($total_items);
+        $this->perpage = intval($items_perpage);
+        $this->current = intval($current_start);
+        $this->extra = $extra_arg;
+        if ($extra_arg != '' && (substr($extra_arg, - 5) != '&amp;' || substr($extra_arg, - 1) != '&')) {
+            $this->extra = '&amp;' . $extra_arg;
+        }
+        $this->url = $_SERVER['PHP_SELF'] . '?' . trim($start_name) . '=';
+    }
+
+    /**
+     * Create text navigation
+     *
+     * @param integer $offset
+     * @param string $size of pagination(Value: 'large', '', 'small', 'mini')
+     * @param string $align of pagination(Value: 'right', 'centered')
+     * @param string $prev_text text for previous
+     * @param string $next_text text for next
+     * @return string
+     */
+    public function renderNav($offset = 4, $size = "", $align = "right", $prev_text = "&laquo;", $next_text = "&raquo;")
+    {
+        $xoopsTpl = new XoopsTpl();
+        $ret = '';
+        $nav = array();
+        if ($this->total <= $this->perpage) {
+            return $ret;
+        }
+        if(($this->total != 0) && ($this->perpage != 0)) {
+            $total_pages = ceil($this->total / $this->perpage);
+            if ($total_pages > 1) {
+                $prev = $this->current - $this->perpage;
+                if ($prev >= 0) {
+                    $xoopsTpl->assign('prev_text', $prev_text);
+                    $xoopsTpl->assign('prev_url', $this->url . $prev . $this->extra);
+                }
+                $last = 0;
+                $last_text = '';
+                $last_url = '';
+                $first = 0;
+                $first_text = '';
+                $first_url = '';
+                $counter = 1;
+                $current_page = intval(floor(($this->current + $this->perpage) / $this->perpage));
+                while ($counter <= $total_pages) {
+                    if ($counter == $current_page) {
+                        $nav['text'] = $counter;
+                        $nav['url'] = '';
+                        $nav['active'] = 0;
+                    } elseif (($counter > $current_page - $offset && $counter < $current_page + $offset) || $counter == 1 || $counter == $total_pages) {
+                        if ($counter == $total_pages && $current_page < $total_pages - $offset) {
+                            $nav['text'] = '...';
+                            $nav['url'] = '';
+                            $nav['active'] = 0;
+                            $last = 1;
+                            $last_text = $counter;
+                            $last_url = $this->url . (($counter - 1) * $this->perpage) . $this->extra;
+                        } else {
+                            $nav['text'] = $counter;
+                            $nav['url'] = $this->url . (($counter - 1) * $this->perpage) . $this->extra;
+                            $nav['active'] = 1;
+                        }
+                        if ($counter == 1 && $current_page > 1 + $offset){
+                            $nav['text'] = '...';
+                            $nav['url'] = '';
+                            $nav['active'] = 0;
+                            $first = 1;
+                            $first_text = $counter;
+                            $first_url = $this->url . (($counter - 1) * $this->perpage) . $this->extra;
+                        }
+                    }
+                    $xoopsTpl->append_by_ref('xo_nav', $nav);
+                    unset($nav);
+                    $counter ++;
+                }
+                $xoopsTpl->assign('last', $last);
+                $xoopsTpl->assign('last_text', $last_text);
+                $xoopsTpl->assign('last_url', $last_url);
+                $xoopsTpl->assign('first', $first);
+                $xoopsTpl->assign('first_text', $first_text);
+                $xoopsTpl->assign('first_url', $first_url);
+
+                $next = $this->current + $this->perpage;
+                if ($this->total > $next) {
+                    $xoopsTpl->assign('next_text', $next_text);
+                    $xoopsTpl->assign('next_url', $this->url . $next . $this->extra);
+                }
+            }
+        }
+        if ($size != ''){
+            $size = ' pagination-' . $size;
+        }
+        $xoopsTpl->assign('size', $size);
+        $xoopsTpl->assign('align', ' pagination-' . $align);
+        $xoopsTpl->assign('pagination_nav', true);
+        $ret = $xoopsTpl->fetch('db:system_pagenav.html');
+        $xoopsTpl->clear_assign('xo_nav');
+        return $ret;
+    }
+
+    /**
+     * Create a navigational dropdown list
+     *
+     * @param boolean $showbutton Show the "Go" button?
+     * @return string|false
+     */
+    public function renderSelect($align = "right", $showbutton = false)
+    {
+        $xoopsTpl = new XoopsTpl();
+        $ret = '';
+        if ($this->total < $this->perpage) {
+            return $ret;
+        }
+        $total_pages = ceil($this->total / $this->perpage);
+        if ($total_pages > 1) {
+            $counter = 1;
+            $current_page = intval(floor(($this->current + $this->perpage) / $this->perpage));
+            while ($counter <= $total_pages) {
+                $select['text'] = $counter;
+                $select['value'] = $this->url . (($counter - 1) * $this->perpage) . $this->extra;
+                if ($counter == $current_page) {
+                    $select['selected'] = 1;
+                } else {
+                    $select['selected'] = 0;
+                }
+                $xoopsTpl->append_by_ref('xo_select', $select);
+                unset($select);
+                $counter ++;
+            }
+        }
+        $xoopsTpl->assign('onchange', "location=this.options[this.options.selectedIndex].value;");
+        $xoopsTpl->assign('pagination_select', true);
+        $xoopsTpl->assign('showbutton', $showbutton);
+        $xoopsTpl->assign('align', ' pagination-' . $align);
+        $ret = $xoopsTpl->fetch('db:system_pagenav.html');
+        $xoopsTpl->clear_assign('xo_select');
+        return $ret;
+    }
+
+    /**
+     * Create navigation with images
+     *
+     * @param integer $offset
+     * @return string|false
+     */
+    public function renderImageNav($offset = 4)
+    {
+        $xoopsTpl = new XoopsTpl();
+        $xoopsTpl->deprecated('renderImageNav() is deprecated since 2.6.0. Please use renderNav()');
+        return $this->renderNav($offset);
+    }
+}
---
+++ xoops-2.5.8_timgno/htdocs/class/xoopsform/formtextdateselect.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * XOOPS form element
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @package         kernel
+ * @subpackage      form
+ * @since           2.0.0
+ * @author          Kazumi Ono (AKA onokazu) http://www.myweb.ne.jp/, http://jp.xoops.org/
+ * @version         $Id: formtextdateselect.php 12537 2014-05-19 14:19:33Z beckmi $
+ */
+
+defined('XOOPS_ROOT_PATH') ||  die("XOOPS root path not defined");
+
+/**
+ * A text field with calendar popup
+ */
+
+class XoopsFormTextDateSelect extends XoopsFormText
+{
+
+    /**
+     * @param     $caption
+     * @param     $name
+     * @param int $size
+     * @param int $value
+     */
+    function XoopsFormTextDateSelect($caption, $name, $size = 15, $value = 0)
+    {
+        $value = !is_numeric($value) ? time() : intval($value);
+        $value = ($value == 0) ? time() : $value;
+        $this->XoopsFormText($caption, $name, $size, 25, $value);
+    }
+
+    /**
+     * @return string
+     */
+    function render()
+    {
+        static $included = false;
+        $ele_name = $this->getName();
+        $ele_value = $this->getValue(false);
+        if (is_string($ele_value)) {
+            $display_value = formatTimestamp($ele_value, _SHORTDATESTRING);
+            $ele_value = time();
+        } else {
+            $display_value = date(_SHORTDATESTRING, $ele_value);
+        }
+
+        if (is_object($GLOBALS['xoTheme'])) {
+            if (!$included) {
+                $included = true;
+                $GLOBALS['xoTheme']->addScript('','', '
+                    $(function() {
+						$( ".datepicker" ).datepicker({ $.datepicker.regional[ "'. _LANGCODE .'" ] });
+						$( ".datepicker" ).datepicker({ 
+							showWeek: true,
+							firstDay: 1,
+							showButtonPanel: true,
+							showOtherMonths: true,
+							selectOtherMonths: true
+						});
+					});
+                ');
+            }
+        }
+        return "<input type='text' name='" . $ele_name . "' id='" . $ele_name . "' class='datepicker' size='" . $this->getSize() . "' value='" . $display_value . "'" . $this->getExtra() . " />";
+    }
+}
